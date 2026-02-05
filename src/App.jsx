e2800942f22587c7f4a2fffcde93f6f63562ced7
HansikaSubhashini <hansikasubhashini50@gmail.com>
2026-02-05 12:46:42 +0530

    Using motion for the anime and develop the mobile application

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import { useState } from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
+
 
 import Loader from "./Components/Loader";
 import ErrorMsg from "./Components/ErrorMsg";
 import SearchBar from "./Components/SearchBar";
 import WeatherCard from "./Components/WeatherCard";
+import axios from "axios";
+import bgImage from "./assets/bg.jpg";
 
-function App() {
+
+function App() 
+
+{
   // State 
   const [city, setCity] = useState(""); 
   const [weather, setWeather] = useState(null); 
   const [loading, setLoading] = useState(false); 
   const [error, setError] = useState("");   
+  
   
   
   console.log(`My city is ${city}`)
@@ -21,50 +28,88 @@ function App() {
   const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"; //  do the import from .env
 
   // Fetch weather data
-  const handleSearch = async () => {
-    if (!city) {
-      setError("Please enter a city name");
-      
-      return(<ErrorMsg></ErrorMsg>);
-    }
+  const handleSearch = async () => { //handleSearch created in a App and sent it for the SearchBar as a props. 
 
-    // Reset before fetching
+    if (!city)
+       {
+
+      setError("Please enter a city name");
+
+       return;
+       }
+       if (loading) return;
+
+  // Reset before fetching
     setLoading(true);
     setError("");
     setWeather(null);
-
+   
     try {
-      const response = await fetch(
+      const response = await axios.get (
         `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
       );
+    
+      const data =  response.data;
 
-      if (!response.ok) {
-        throw new Error("City not found");
-      }
-
-      const data = await response.json();
       setWeather(data); // Save data to state
-    } catch (err) {
+    } 
+    catch (err) 
+    {
       setError(err.message);
-    } finally {
+    } 
+    finally
+    {
       setLoading(false);
     }
   };
+return (
+  <Box
+    sx={{
+      position: "fixed",
+      inset: 0,
+      minHeight: "100vh",
+      width: "100%",
+      overFlow: "hidden",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundImage: `linear-gradient(
+        rgba(0,0,0,0.5),
+        rgba(0,0,0,0.5)
+      ), url(${bgImage})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    }}
+  >
+    <Container maxWidth="sm" disableGutters>
+      <Typography
+        variant="h4"
+        align="center"
+        fontWeight="bold"
+        gutterBottom
+        color="white"
+      >
+      Weather Application
+      </Typography>
 
-  return (
-    <Container maxWidth="sm" sx={{ mt: 5 }}>
-      
-      <SearchBar city={city} setCity={setCity} onSearch={handleSearch} />
+      <SearchBar
+        city={city}
+        setCity={setCity}
+        onSearch={handleSearch}
+      />
 
       {loading && <Loader />}
-
       {error && <ErrorMsg message={error} />}
-
-   
       {weather && <WeatherCard data={weather} />}
 
+      {!weather && !loading && !error && (
+        <Typography align="center" color="white">
+          Search for a city to view weather details
+        </Typography>
+      )}
     </Container>
-  );
+  </Box>
+);
 }
-
 export default App;
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,24 +1,75 @@
-import { TextField , Button } from "@mui/material";
+import { TextField , Button, Box, InputAdornment } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 
 function SearchBar({ city, setCity, onSearch }){
  
     return(
         <>
+        
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+            mt: 2,
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100%",
+            maxWidth: 600,
+            mx: "auto",
+
+       }}
+
+       >
+
         <TextField
-            label="Enter City"
+            fullWidth
+            variaent="outlined"
             value={city}
             onChange={(e) => setCity(e.target.value)}
-
+            InputProps={{
+                startAdornment: (
+                    <InputAdornment position="start">
+                        <SearchIcon color="action" />
+                    </InputAdornment>
+                ),
+        
+                
+                sx: {
+                  borderRadius: "50px",
+                  backgroundColor: "#f5f5f5",
+                  "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "#ccc",
+                  },
+                  "&:hover .MuiOutlinedInput-notchedOutline":
+                  {
+                    borderColor: "#1976d2",
+                  },
+                },
+              }}
             />
 
-            <Button variant="contained" onClick={onSearch}>
-                Search
+            <Button 
+             variant="contained" 
+             onClick={onSearch}
+             sx={{
+                
+                px: 3,
+                py: 1.5,
+                borderRadius: "50px",
+                background: "linear-gradient(45deg, #1976d2, #42a5f5)",
+                boxShadow: "0 4px 10px rgba(0,0,0,0.2)",
+                textTransform: "none",
+                "&:hover" : {
+                    background: "linear-gradient(45deg, #1565c0, #64b5f6)",
+                     },
+                  }} 
+             >
+                  Search
             </Button>
+            </Box>
         </>
     );
-
-
 }
 
 export default SearchBar;
